35a53678a29b8e1d09fa6a59a5ecc6a37b1bc799
Abdul Wadood <49836993+RANAABDULWADOODRAJPOOT@users.noreply.github.com>
2023-10-17 19:38:11 +0500

    Update Refresh

--- api/auth/refresh_token.js
+++ api/auth/refresh_token.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-router.post('/', (req, res) => {
+router.get('/', (req, res) => {
   
   const refreshToken = req?.cookies?.refresh_token;
 
